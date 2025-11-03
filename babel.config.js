8505dc82928c698b8b4334d8e54cc345d8b73022
Prof. Fernando Pinéo <pf1963@fiap.com.br>
2025-11-03 09:44:33 -0300

    Removido erro de conflito de dependências

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
-module.exports = {
-  presets: [
-    "module:metro-react-native-babel-preset", // Preset React Native
-    "@babel/preset-typescript",               // Para TS
-  ],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    // Use the expo preset (recommended for Expo SDK 50+). It includes
+    // the transforms needed by expo-router so we don't need the
+    // deprecated "expo-router/babel" plugin.
+    presets: ["babel-preset-expo"],
+  };
 };
